83754d3494264736bd94c750014e50ac6975d613
saurabhsinha09 <sinha.saurabh06@gmail.com>
2022-08-12 22:11:42 +0530

    subnet,fw and storage modules

--- gcp/tf_modules/compute-composite-repo/main.tf
+++
@@ -1,8 +0,0 @@
-/**
-* # Terraform Persistent Compute Instance Module
-*
-* ## Description
-* Create a GCE compute instance with a service account and static ip address. 
-* The module will also create supplemental disks and a snapshot policy for the boot disk and supplemental disks.
-*
-*/
